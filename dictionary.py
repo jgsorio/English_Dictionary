8eee7e7049f127f71531a767a5dc2aa16559cbaf
José Guilherme Sório <joseguilhermesorio@gmail.com>
2021-04-22 20:55:26 -0300

    Melhoria no metodo de busca com mais assertividade

--- dictionary.py
+++ dictionary.py
@@ -1,24 +1,41 @@
 import json
+from difflib import get_close_matches
 
 dados = json.load(open('data.json'))
 
 answer = input('What word do you wanna search? ')
 
+'''
+Método que retorna com 80% de assertividade as palavras similares
+dentro do dicionario, caso o cliente digite algo errado
+@author José Guilherme
+'''
+
+
+def filter_word(word):
+    return get_close_matches(word, dados.keys(), cutoff=0.8)
+
 
 def word_test(word):
-    if word.upper() in dados:
-        return dados[word.upper()]
-    elif word.title() in dados:
-        return dados[word.title()]
-    elif word.lower() in dados:
-        return dados[word.lower()]
-    elif word.capitalize() in dados:
-        return dados[word.capitalize()]
+    words = filter_word(word)
+    if len(words) > 0:
+        for word in words:
+            if word.upper() in dados:
+                return dados[word.upper()]
+            elif word.title() in dados:
+                return dados[word.title()]
+            elif word.lower() in dados:
+                return dados[word.lower()]
+            elif word.capitalize() in dados:
+                return dados[word.capitalize()]
     else:
-        return ['The search returned no results']
+        return False
 
 
 while answer != '':
-    for (indice, result) in enumerate(word_test(answer)):
-        print(f'{indice} - {result}')
+    if type(word_test(answer)) != bool:
+        for (indice, result) in enumerate(word_test(answer)):
+            print(f'{indice} - {result}')
+    else:
+        print('The search return no results!')
     answer = input('What word do you wanna search? ')
