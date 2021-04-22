aa1fe519a5a70074cfaab608f8ea9b970f29479a
José Guilherme Sório <joseguilhermesorio@gmail.com>
2021-04-22 18:46:29 -0300

    Fix, transformando a palavra em lower para encontrar match

--- dictionary.py
+++ dictionary.py
@@ -6,7 +6,7 @@ answer = input('What word do you wanna search? ')
 
 while answer != '':
     try:
-        for result in dados[answer]:
+        for result in dados[answer.lower()]:
             print(result)
     except:
         print('The search returned no results')
