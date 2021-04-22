dedd2745a759c63379e42dc995af12f444119a0a
José Guilherme Sório <joseguilhermesorio@gmail.com>
2021-04-22 19:12:45 -0300

    Fix, testando todas as possibilidades das palavras para encontrar match

--- dictionary.py
+++ dictionary.py
@@ -4,11 +4,21 @@ dados = json.load(open('data.json'))
 
 answer = input('What word do you wanna search? ')
 
-while answer != '':
-    try:
-        for result in dados[answer.lower()]:
-            print(result)
-    except:
-        print('The search returned no results')
-    answer = input('What word do you wanna search? ')
 
+def word_test(word):
+    if word.upper() in dados:
+        return dados[word.upper()]
+    elif word.title() in dados:
+        return dados[word.title()]
+    elif word.lower() in dados:
+        return dados[word.lower()]
+    elif word.capitalize() in dados:
+        return dados[word.capitalize()]
+    else:
+        return 'The search returned no results'
+
+
+while answer != '':
+    for result in word_test(answer):
+        print(result)
+    answer = input('What word do you wanna search? ')
