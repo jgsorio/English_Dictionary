191e217b9b6aab2495fd764eb154b36f223b2837
José Guilherme Sório <joseguilhermesorio@gmail.com>
2021-04-22 21:40:08 -0300

    Melhoria nos filtros

--- dictionary.py
+++ dictionary.py
@@ -5,6 +5,21 @@ dados = json.load(open('data.json'))
 
 answer = input('What word do you wanna search? ')
 
+
+def word_test(word):
+    if word.upper() in dados:
+        return dados[word.upper()]
+    elif word.title() in dados:
+        return dados[word.title()]
+    elif word.lower() in dados:
+        return dados[word.lower()]
+    elif word.capitalize() in dados:
+        return dados[word.capitalize()]
+    else:
+        return False
+
+
+
 '''
 Método que retorna com 80% de assertividade as palavras similares
 dentro do dicionario, caso o cliente digite algo errado
@@ -13,29 +28,19 @@ dentro do dicionario, caso o cliente digite algo errado
 
 
 def filter_word(word):
-    return get_close_matches(word, dados.keys(), cutoff=0.8)
-
-
-def word_test(word):
-    words = filter_word(word)
-    if len(words) > 0:
-        for word in words:
-            if word.upper() in dados:
-                return dados[word.upper()]
-            elif word.title() in dados:
-                return dados[word.title()]
-            elif word.lower() in dados:
-                return dados[word.lower()]
-            elif word.capitalize() in dados:
-                return dados[word.capitalize()]
+    matches = get_close_matches(word, dados.keys(), cutoff=0.8)
+    if len(matches) > 1:
+        question = input(f'Did you mean {matches[0]}? Press y or n ')
+        if question.lower() == 'y':
+            return str(matches[0])
+        else:
+            return False
     else:
-        return False
-
+        return str(matches[0])
 
 while answer != '':
-    if type(word_test(answer)) != bool:
-        for (indice, result) in enumerate(word_test(answer)):
+    word = filter_word(answer)
+    if type(word) != bool:
+        for (indice, result) in enumerate(word_test(word)):
             print(f'{indice} - {result}')
-    else:
-        print('The search return no results!')
     answer = input('What word do you wanna search? ')
